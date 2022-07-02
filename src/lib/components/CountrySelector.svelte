932a8aee0b4c7465aa2dc50a09e7bff97f3d2ca1
Holden Greene <holdengreene26@gmail.com>
2022-07-01 19:08:40 -0600

    Extracted country selector component

---
+++ src/lib/components/CountrySelector.svelte
@@ -0,0 +1,11 @@
+<script>
+	import countries from '$lib/json/countries.json';
+</script>
+
+<label for="country">Country</label>
+<input id="country" list="country-list" name="country" autocomplete="off" required />
+<datalist id="country-list">
+	{#each countries as country}
+		<option value={country.name} />
+	{/each}
+</datalist>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <script>
-	import countries from '$lib/json/countries.json';
+	import CountrySelector from '$lib/components/CountrySelector.svelte';
 
 	export let countryCode = '';
 </script>
@@ -10,13 +10,7 @@
 <h2>Country Code: {countryCode}</h2>
 
 <form method="post">
-	<label for="country">Country</label>
-	<input id="country" list="country-list" name="country" autocomplete="off" required />
-	<datalist id="country-list">
-		{#each countries as country}
-			<option value={country.name} />
-		{/each}
-	</datalist>
+	<CountrySelector />
 
 	<button type="submit">Submit</button>
 </form>
